341bb9d6e952205ca48b5a67022c3739afd11e5b
scgitstuff <67354101+scgitstuff@users.noreply.github.com>
2025-12-05 15:07:46 -0500

    9.7

--- stufflist.sh
+++ stufflist.sh
@@ -3,11 +3,13 @@ kubectl apply -f api-pvc.yaml
 kubectl apply -f api-configmap.yaml
 kubectl apply -f crawler-configmap.yaml
 kubectl apply -f web-configmap.yaml
+kubectl apply -f testram-configmap.yaml
 
 kubectl apply -f api-deployment.yaml
 kubectl apply -f crawler-deployment.yaml
 kubectl apply -f web-deployment.yaml
 kubectl apply -f testcpu-deployment.yaml
+kubectl apply -f testram-deployment.yaml
 
 kubectl apply -f api-service.yaml
 kubectl apply -f crawler-service.yaml
